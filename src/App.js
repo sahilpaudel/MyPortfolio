3daf3141647549ba5a3d4d7f421add78992db838
Sahil Paudel <sahil.p@pharmeasy.in>
2020-05-10 19:50:17 +0530

    -Adding google analytics

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from "react";
+import ReactGA from 'react-ga';
+
 import "./styles.css";
 import SideNavigation from "./navigation";
 
 export default function App() {
+
+  React.useEffect(() => {
+    ReactGA.initialize('UA-166063982-1');
+    ReactGA.pageview('/portfolio');
+  }, [])
+
   return (
     <React.Fragment>
       <SideNavigation />
